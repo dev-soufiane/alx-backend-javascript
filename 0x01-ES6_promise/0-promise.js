0355d474b017f88438f09a9ade951f798d7ab1fc
dev-soufiane <dev.sofyane@gmail.com>
2024-06-01 00:52:28 +0100

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,3 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    const checker = true;
-    if (checker) {
-      resolve('Success');
-    } else {
-      reject(new Error('Failed'));
-    }
-  }).catch((error) => console.error(error));
-}
+    return new Promise(() => {});
+  }
