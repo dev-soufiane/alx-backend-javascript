90558a4a8534569077bf4fb30e80b0b2a9a61074
dev-soufiane <dev.sofyane@gmail.com>
2024-06-01 00:14:52 +0100

    Update 100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,9 +1,9 @@
-import Car from './10-car';
+import Car from './10-car.js';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
-    this.range = range;
+    this._range = range; // Store range in underscore attribute
   }
 
   get range() {
@@ -15,8 +15,9 @@ export default class EVCar extends Car {
   }
 
   cloneCar() {
-    const Species = super.constructor[Symbol.species];
-
-    return new Species();
+    const Species = this.constructor[Symbol.species]; // Access Species directly from this.constructor
+    // Pass necessary arguments to Species constructor
+    return new Species(this._brand, this._motor, this._color);
   }
 }
+
